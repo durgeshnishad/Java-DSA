f4b62dcdd4d53a3aef3ee2b675988e7b378f7266
Durgesh Nishad <dnishad042@gmail.com>
2023-05-24 13:53:48 +0530

    subarray re-written

--- Arrays/Subarray.java
+++ Arrays/Subarray.java
@@ -12,16 +12,11 @@ public class Subarray{
             n[i]=sc.nextInt();
         }
         System.out.println("Subarray of the above array are");
-        for(int j=0;j<size;j++){
-            for(int k=j;k<size;k++){
-                System.out.print("["+n[j]);
-                if(j==k){
-                    System.out.print("]");
-                    continue;
-                }
-                else
+        for(int i=0;i<size;i++){
+            for(int j=i;j<size;j++){
+                for(int k=i;k<=j;k++)
                     System.out.print(n[k]);
-            System.out.print("]");
+                System.out.println();
             }
             System.out.println();
         }
